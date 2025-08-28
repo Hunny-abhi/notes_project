b6a162e984177c5120e73ca84d615083259348d7
Hunny-abhi <hunny.abhi6552@gmail.com>
2025-08-28 16:22:26 +0530

    mongoUri

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-
-// const appp = express();
-
-// app.get("/", (req, res) => {
-//   res.send("Welcome to the Notes API");
-// });
-// module.exports = appp;
